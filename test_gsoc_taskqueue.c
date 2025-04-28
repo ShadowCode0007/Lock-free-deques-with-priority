5511fef189daab395fc172f23c8f05277021402b
Amogh Hegde <amoghah@linux-01.andrew.cmu.edu>
2025-04-28 13:57:54 -0400

    Added metrics to debug logs

--- test_gsoc_taskqueue.c
+++ test_gsoc_taskqueue.c
@@ -56,13 +56,19 @@ void *parallel_push_pop_take(void *s)
   }
 
   int current_priority = 0;
+  double start_time = gettimeofday_sec();
+
+  int steal_count_per_priority = 0;
+  int steal_count_total = 0;
 
   while (1)
   {
     gsoc_task task;
 
-    if (current_priority > 2)
-      break;
+    if (current_priority > 2) {
+	 fprintf(stderr, "\nTasks stolen by CPU%d in total is %d \n", sched_getcpu(), steal_count_total);
+         break;
+    }
 
     task = gsoc_taskqueue_set_pop(data->taskqs, current_priority);
 
@@ -95,6 +101,8 @@ void *parallel_push_pop_take(void *s)
            printf("%lld is taken by CPU%d from CPU%d (priority %d)\n",
                  task.test_id, sched_getcpu(), i, current_priority);
            execute_task(task);
+	   steal_count_total++;
+	   steal_count_per_priority++;
 	   found = 1;
            break;
          }
@@ -104,6 +112,13 @@ void *parallel_push_pop_take(void *s)
       if (found == 0)
       {
          current_priority++;
+	 double end_time = gettimeofday_sec();
+  	 fprintf(stderr, "%f sec taken for priority %d tasks in CPU%d\n", end_time - start_time, current_priority - 1, sched_getcpu());
+	 start_time = end_time;
+
+	 steal_count_per_priority = 0;
+
+	 fprintf(stderr, "\nTasks stolen by CPU %d of priority %d is %d \n", sched_getcpu(), current_priority - 1, steal_count_per_priority);
          printf("(CPU%d) current_priority has been incremented to %d\n", sched_getcpu(), current_priority);
       }
     }
@@ -171,7 +186,7 @@ int main()
     pthread_join(tids[i], NULL);
 
   double t2 = gettimeofday_sec();
-  fprintf(stderr, "%f sec elapsed for parallel_push_pop_take()\n", t2 - t1);
+  fprintf(stderr, "\n%f sec elapsed for all the tasks in total()\n", t2 - t1);
 
   return 0;
 }
